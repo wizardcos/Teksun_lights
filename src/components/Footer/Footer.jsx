fe37ecc1c1612aa015bebb67e9d19f35a013ba5d
wizardcos <taharajpoot204@gmail.com>
2024-09-10 17:02:06 +0500

    aded the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,34 +1,34 @@
 import React from "react";
 import { footerVariants, staggerChildren } from "../../utils/motion";
 import css from "./Footer.module.scss";
-import {motion} from 'framer-motion'
+import { motion } from "framer-motion";
 const Footer = () => {
   return (
     <motion.section
-    variants={staggerChildren}
-    initial="hidden"
-    whileInView="show"
-    viewport={{ once: false, amount: 0.25 }}
-    className={`paddings ${css.wrapper}`}>
-      
-      
+      variants={staggerChildren}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`paddings ${css.wrapper}`}
+    >
       <motion.div
-      variants={footerVariants}
-      className={`innerWidth yPaddings flexCenter ${css.container}`}>
+        variants={footerVariants}
+        className={`innerWidth yPaddings flexCenter ${css.container}`}
+      >
         <div className={css.left}>
           <span className="primaryText">
             Let's make something <br />
             amazing together.
           </span>
           <span className="primaryText">
-            Start by <a href="mailto:zainkeepscode@gmail.com">saying hi</a>
+            Start by <a href="mailto:hamza@teksunlights.com">saying hi</a>
           </span>
         </div>
 
         <div className={css.right}>
           <div className={css.info}>
             <span className="secondaryText">Information</span>
-            <p>145 New York, FL 5467, USA</p>
+            <p>Bhagewaal road FSD, 38000 , Pakistan</p>
           </div>
           <ul className={css.menu}>
             <li>Services</li>
